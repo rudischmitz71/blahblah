74bc3ebc0fff578aeac03cab7825a2df21470c04
Rudi Schmitz <rudischmitz@gmail.com>
2023-09-26 08:24:25 -0500

    0-runme.sh

---
+++ 0-runme.sh
@@ -0,0 +1,11 @@
+wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/1-base.sh
+wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/2-wordpress.sh
+wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/3-O365SPOexceldisplay.sh
+
+sudo chmod +x 1-base.h
+sudo chmod +x 2-wordpress.sh
+sudo chmod +x 3-O365SPOexceldisplay.sh
+
+1-base.h
+2-wordpress.sh
+3-O365SPOexceldisplay.sh
