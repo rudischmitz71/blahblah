308ea3b29f6a9e51990acfd61d1b4ada63e7bba2
Rudi Schmitz <rudischmitz@gmail.com>
2023-09-27 20:02:46 -0500

    0-tablepi.sh

--- 0-tablepi.sh
+++ 0-tablepi.sh
@@ -4,9 +4,7 @@ wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/3-O365SPOexce
 wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/4-finalize-tablepi.sh
 cp /home/$USER/4-finalize-tablepi.sh /home/$USER/Desktop/4-finalize-tablepi.sh
 
-sudo chmod +x /home/$USER/1-base.sh
-sudo chmod +x /home/$USER/2-wordpress.sh
-sudo chmod +x /home/$USER/3-O365SPOexceldisplay.sh
+sudo chmod +x /home/$USER/*.sh
 
 /home/$USER/1-base.sh
 /home/$USER/2-wordpress.sh
