0afc14964261ecb4a1ea2a085f52786ccad8c111
Rudi Schmitz <rudischmitz@gmail.com>
2023-10-01 13:54:20 -0500

    Update 0-tablepi.sh

--- 0-tablepi.sh
+++ 0-tablepi.sh
@@ -1,11 +1,4 @@
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/1-base.sh
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/2-wordpress.sh
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/3-O365SPOexceldisplay.sh
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/4-finalize-tablepi.sh
+wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/1-tablepi.sh
 cp /home/$USER/4-finalize-tablepi.sh /home/$USER/Desktop/4-finalize-tablepi.sh
-
 sudo chmod +x /home/$USER/*.sh
-
-/home/$USER/1-base.sh
-/home/$USER/2-wordpress.sh
-/home/$USER/3-O365SPOexceldisplay.sh
+/home/$USER/1-tablepi.sh
