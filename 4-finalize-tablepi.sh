58ed7b9ac3313fc1375e715a134022a01f210876
Rudi Schmitz <rudischmitz@gmail.com>
2023-10-02 10:32:15 -0500

    4-finalize-tablepi.sh

--- 4-finalize-tablepi.sh
+++ 4-finalize-tablepi.sh
@@ -3,6 +3,9 @@ cd /var/www/html
 sudo wp plugin install --activate "fullwidth-templates" --allow-root
 sudo wp plugin install --activate "wpdatatables" --allow-root
 
+#fix wordpress folder permissions
+sudo chown www-data:www-data /var/www/html/wp-content/upgrade/
+
 cd ~
 # refresh every 15 minutes
 #write out current crontab
