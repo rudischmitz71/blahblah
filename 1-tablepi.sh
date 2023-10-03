fc3a18816d35a1f24069a55f6253d65ba435f7ac
Rudi Schmitz <rudischmitz@gmail.com>
2023-10-03 09:42:14 -0500

    1-tablepi.sh

--- 1-tablepi.sh
+++ 1-tablepi.sh
@@ -15,8 +15,8 @@ read -p 'What is the excel filename: yourfilename.xlsx" : ' o365filenamepost
 while true; do
     read -p "Is this a regular SharePoint Site hit S or is it a Teams Sharepoint Site hit T?" st
     case $st in
-        [Ss]* ) $o365filename="${o365filenamepost}"; break;;
-        [Tt]* ) $o365filename="General/${o365filenamepost}"; break;;
+        [Ss]* ) $o365filename="$o365filenamepost"; break;;
+        [Tt]* ) $o365filename="General/$o365filenamepost"; break;;
         * ) echo "Please answer S or T.";;
     esac
 done
