95e129b7be92d5822ef1cdab13ca6a43b2596e9e
Ugotapi <rudischmitz@gmail.com>
2023-10-04 13:48:09 -0500

    0-tablepi.sh

--- 0-tablepi.sh
+++ 0-tablepi.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/1-tablepi.sh
 wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/4-finalize-tablepi.sh
 cp /home/$USER/4-finalize-tablepi.sh /home/$USER/Desktop/4-finalize-tablepi.sh
