39d1e07e7f3171e81b6716678060ae30b3be8b37
Ugotapi <rudischmitz@gmail.com>
2023-11-05 16:24:25 -0600

    Update and rename 1-tablepi.sh to 1-calendarpi.sh

--- 1-calendarpi.sh
+++ 1-calendarpi.sh
@@ -3,32 +3,13 @@ ifconfig
 # Ask the user for info
 read -p 'What is your ip address in a url - http://10.10.10.10 ' webvar
 read -p 'What password to use for the wordpress user wpuser: ' passvar
-read -p 'What is your .onmicrosoft.com Domain Name?  DOMAINNAME.onmicrosoft.com: ' spovar
-read -p 'What sharepoint sitename: ' sitenamevar
-read -p 'What Office365 username: ' o365username
-read -p 'What Office365 password: ' o365userpassword
-read -p 'What is the excel filename: yourfilename.xlsx : ' o365filenamepost
-
-
-# Teams or regular sharepoint the urls are different
-while true; do
-    read -p "Is this a Sharepoint Online site or a Teams Sharepoint Site? s or t " st
-    case $st in
-        [Ss]* ) o365filename="$o365filenamepost";break;;
-        [Tt]* ) o365filename='General/'$o365filenamepost;break;;
-        * ) echo "Please answer s or t";;
-    esac
-done
+read -p 'What Office365 ics calendar link: ' o365cal
 
 
 echo local ip address of your pi: $webvar
 echo wordpress database password: $passvar
-echo Office 365 domain name: $spovar
-echo Sharepoint site name: $sitenamevar
-echo Office 365 username: $o365username
-echo Office365 user password: $o365userpassword
-echo Excel file: $o365filename
-echo This is your sharepoint url https://$spovar.sharepoint.com/sites/$sitenamevar
+echo Office365 ics calendar url: $o365cal
+
 while true; do
     read -p "Is the above info correct? " yn
     case $yn in
@@ -165,10 +146,8 @@ sudo mysql -e "CREATE DATABASE wordpressdb";
 sudo mysql -e "CREATE USER wpuser@localhost IDENTIFIED BY '$passvar'";
 sudo mysql -e "GRANT ALL ON wordpressdb.* TO wpuser@localhost IDENTIFIED BY '$passvar'";
 
-
 #display xlsx with wordpress local install
 #note this doesnt work for Office365 MFA enabled users or subfolders under sharepoint unless the script is edited.
-
 
 #install wp-cli wordpress cli and some modules
 cd ~
@@ -176,62 +155,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
-#install powershell for Linux on pi4
-sudo apt-get install wget libssl1.1 libunwind8 -y
-sudo mkdir -p /opt/microsoft/powershell/7
-wget -O /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.8/powershell-7.3.8-linux-arm64.tar.gz 
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
-sudo chmod +x /opt/microsoft/powershell/7/pwsh
-sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
-rm /tmp/powershell.tar.gz
-echo "Install-Module -Name PnP.PowerShell -Force" > /home/$USER/temp.ps1
-pwsh /home/$USER/temp.ps1 -NonInteractive
-
-#install python and openpyxl
-sudo apt-get install python3 python3-pip python3-openpyxl -y
-
-#powershell to get file from office365
-#powershell to get file from office365
-echo "Remove-Item -Path \"/home/$USER/*.xlsx\" -Recurse -Force -Confirm:\$false" > /home/$USER/getstuff.ps1 
-echo "Remove-Item -Path \"/home/$USER/*.csv\" -Recurse -Force -Confirm:\$false" >> /home/$USER/getstuff.ps1 
-echo "#Config Variables" >> /home/$USER/getstuff.ps1
-echo "\$SiteURL = \"https://$spovar.sharepoint.com/sites/$sitenamevar\"" >> /home/$USER/getstuff.ps1
-echo "\$FileRelativeURL = \"/sites/$sitenamevar/Shared Documents/$o365filename\"" >> /home/$USER/getstuff.ps1
-echo "\$DownloadPath =\"/home/$USER/\"" >> /home/$USER/getstuff.ps1
-echo "\$username=\"$o365username\"" >> /home/$USER/getstuff.ps1
-echo "\$encpassword = convertto-securestring -String \"$o365userpassword\" -AsPlainText -Force" >> /home/$USER/getstuff.ps1 
-echo "\$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist \$username, \$encpassword" >> /home/$USER/getstuff.ps1
-echo "Connect-PnPOnline -Url \$SiteURL -Credentials \$cred " >> /home/$USER/getstuff.ps1
-echo "Get-PnPContext" >> /home/$USER/getstuff.ps1
-echo "#powershell download file from sharepoint online" >> /home/$USER/getstuff.ps1
-echo "Get-PnPFile -Url \$FileRelativeURL -Path \$DownloadPath -AsFile -FileName \"$o365filenamepost\"" >> /home/$USER/getstuff.ps1 
-echo "#edit xlsx file and get to csv" >> /home/$USER/getstuff.ps1
-echo "python /home/$USER/runme.py" >> /home/$USER/getstuff.ps1
-echo "#copy to apache folder" >> /home/$USER/getstuff.ps1
-echo "sudo cp -f /home/$USER/output.csv /var/www/html" >> /home/$USER/getstuff.ps1
-echo "sudo chown www-data:www-data /var/www/html/output.csv" >> /home/$USER/getstuff.ps1
-
-# create python file to change to excel contents around if needed and save as csv
-cat > /home/$USER/runme.py << EOL
-import openpyxl
-from openpyxl import load_workbook
-wb = load_workbook(filename = '/home/$USER/$o365filenamepost')
-ws = wb.active
-# UNMERGE CELLS ws.unmerge_cells(start_row=1, start_column=1, end_row=1, end_column=10)
-# DELETE A ROW ws.delete_rows(1)
-wb.save('/home/$USER/output.xlsx')
-## XLSX TO CSV
-import openpyxl
-import csv
-wb = openpyxl.load_workbook('/home/$USER/$o365filenamepost')
-sh = wb.active # was .get_active_sheet()
-with open('/home/$USER/output.csv', 'w', newline="") as file_handle:
-    csv_writer = csv.writer(file_handle)
-    for row in sh.iter_rows(): # generator; was sh.rows
-        csv_writer.writerow([cell.value for cell in row])
-EOL
-
-
-read -p "After this reboot 1. Setup wordpress. 2. Then run the 4-finalize-tablepi.sh on the Desktop. Hit Enter key to continue"
+read -p "After this reboot 1. Setup wordpress. 2. Then run the 4-finalize-calendarpi.sh on the Desktop. Hit Enter key to continue"
 sudo reboot
 
